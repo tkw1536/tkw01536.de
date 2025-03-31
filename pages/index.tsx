ace92f0a9a08f8e6bcfe02424ed5b4ec62100e59
Tom Wiesing <tkw01536@gmail.com>
2025-03-31 14:18:37 +0200

    update website with more up-to-date topic

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,117 +4,48 @@ import Head from "next/head";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faMastodon } from '@fortawesome/free-brands-svg-icons'
 
-function ForeignObjectHack({ math, ...rest }: { math: string } & Record<string, unknown>) {
-  return <foreignObject {...rest} dangerouslySetInnerHTML={{__html: math}} />
-}
-
 export default class Home extends React.Component {
   render() {
-    const math1 = `<math id="p1.1.m1.1" alttext="\frac{x+y}{x}" display="inline"><semantics id="p1.1.m1.1a"><mfrac id="p1.1.m1.1.1" xref="p1.1.m1.1.1.cmml"><mrow id="p1.1.m1.1.1.2" xref="p1.1.m1.1.1.2.cmml"><mi id="p1.1.m1.1.1.2.2" xref="p1.1.m1.1.1.2.2.cmml">x</mi><mo id="p1.1.m1.1.1.2.1" xref="p1.1.m1.1.1.2.1.cmml">+</mo><mi id="p1.1.m1.1.1.2.3" xref="p1.1.m1.1.1.2.3.cmml">y</mi></mrow><mi id="p1.1.m1.1.1.3" xref="p1.1.m1.1.1.3.cmml">x</mi></mfrac><annotation-xml encoding="MathML-Content" id="p1.1.m1.1b"><apply id="p1.1.m1.1.1.cmml" xref="p1.1.m1.1.1"><divide id="p1.1.m1.1.1.1.cmml" xref="p1.1.m1.1.1"></divide><apply id="p1.1.m1.1.1.2.cmml" xref="p1.1.m1.1.1.2"><plus id="p1.1.m1.1.1.2.1.cmml" xref="p1.1.m1.1.1.2.1"></plus><ci id="p1.1.m1.1.1.2.2.cmml" xref="p1.1.m1.1.1.2.2">𝑥</ci><ci id="p1.1.m1.1.1.2.3.cmml" xref="p1.1.m1.1.1.2.3">𝑦</ci></apply><ci id="p1.1.m1.1.1.3.cmml" xref="p1.1.m1.1.1.3">𝑥</ci></apply></annotation-xml></semantics></math>`
-    const math2 = `<math><mtext>.</mtext></math>`; 
-
-
     return <>
       <Head>
         <title>Tom Wiesing</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Head>
-      <div className="row" id="header">
-        <div className="col s12">
-          <h2>Tom Wiesing</h2>
-
-          <a href="mailto:tom@tkw01536.de" className="waves-effect waves-light btn blue"><i className="material-icons left">email</i>tom@tkw01536.de</a>
-          &nbsp;
-          <a href="https://github.com/tkw1536" className="btn teal" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
-          &nbsp;
-          <a href="https://mastodon.social/@tkw01536" className="btn teal" target="_blank" rel="me"><FontAwesomeIcon icon={faMastodon} /></a>
+      <header className="row">
+        <div className="col s4">
+          <h3>Tom Wiesing</h3>
         </div>
-      </div>
+        <div className="col s8" style={{paddingTop: '2em', float: 'right'}}>
+            <a href="mailto:tom@tkw01536.de" className="waves-effect waves-light btn blue"><i className="material-icons left">email</i>tom@tkw01536.de</a>
+            &nbsp;
+            <a href="https://github.com/tkw1536" className="btn teal" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+            &nbsp;
+            <a href="https://mastodon.social/@tkw01536" className="btn teal" target="_blank" rel="me"><FontAwesomeIcon icon={faMastodon} /></a>
+        </div>        
+      </header>
 
-      <div className="row">
+      <div className="row" style={{marginTop: '-1em'}}>
         <div className="col s12 l6">
           <div className="card">
             <div className="card-content">
               <img src="/profile.png" width="100" height="100" alt="Profile Image" />
               <p>
-                Hi, my name is Tom Wiesing and I am a <em>PhD candidate</em> in the <em>KWARC Group at FAU Erlangen-N&uuml;rnberg</em>.
+                Hi, my name is Tom Wiesing and I am a <em>PhD candidate</em> in the <em>KWARC Group at FAU Erlangen-N&uuml;rnberg</em> and work at the <em>FAU Competence Center for Research Data and Information</em>
               </p>
+              <br />
               <p>
                 In my free time I like to code and make random websites.
-                I also volunteer for the <em>Jacobs Alumni Association</em>, acting as their <em>Assessor of Information Technology</em>.
               </p>
             </div>
             <div className="card-action">
               <a href="https://guys.wtf/" target="_blank" rel="noreferrer noopener">Collection of Random websites</a>
-              <a href="https://jacobs-alumni.de/" target="_blank" rel="noreferrer noopener">Alumni</a>
+              <a href="https://www.cdi.fau.de/en/" target="_blank" rel="noreferrer noopener">FAU-CDI</a>
               <a href="https://kwarc.info/" target="_blank" rel="noreferrer noopener">KWARC</a>
             </div>
            </div>
 
-            <div className="card">
-              <div className="card-content">
-                <span className="card-title">Research</span>
-                <p className="roman">
-                  Mathematical practice typically consists of four aspects:
-                </p>
-                  <ol className="roman">
-                      <li>
-                          making <em>inferences</em> about mathematical knowledge;
-                      </li>
-                      <li>
-                          writing <em>narrating</em> content about specific knowledge;
-                      </li>
-                      <li>
-                          generating large amounts of informormation and <em>concretizing</em> it in some form of database; and
-                      </li>
-                      <li>
-                          making <em>computations. </em>
-                      </li>
-                  </ol>
-                <p className="roman">
-                  These are connected by a central ontology.
-                </p>
-                <p>
-                  My research topic is how to digitally support such <em>tetrapodal mathematics</em> with a focus on search.
-                  I am performing this research using research systems such as <em>MathHub</em>, <em>MathWebSearch</em> and <em>MathDataHub</em>.
-                </p>
-                <p>
-                  I am particularly interested in mathematical datasets, and how these are typicalled searched through in practice.
-                  Datasets can include anything from simple listings of interesting mathematical objects; via corpora of papers written in LaTeX; to strictly defined objects in some logical system.
-                </p>
-                <p>
-                  Search practices consists of queries and how to evaluate them. 
-                  Queries can range from simple things like <i>find all groups with a specific generator</i> to more complex things like <i>find all papers that cite a paper which contains a formula that unifies with 
-                  <svg role="math" xmlns="http://www.w3.org/2000/svg" width="100" height="30" style={{
-                    'marginTop': -15,
-                    position: 'relative',
-                    top: 15,
-                  }}>
-                    <switch>
-                      <ForeignObjectHack requiredExtensions="http://www.w3.org/1998/Math/MathML" width="70" height="30" math={math1} />
-                      <g>
-                        <text y="15" fontSize="16">(x + y) / x</text>
-                        <text x="70" y="15" fontSize="14">.</text>
-                      </g>
-                    </switch>
-                    <switch>
-                      <ForeignObjectHack requiredExtensions="http://www.w3.org/1998/Math/MathML" width="70" height="30" x="35" y="0" math={math2} />
-                    </switch>
-                  </svg>
-                </i>
-                </p>
-              </div>
-              <div className="card-action">
-                <a href="https://mathhub.info/" target="_blank" rel="noreferrer noopener">MathHub</a>
-                <a href="https://data.mathhub.info/" target="_blank" rel="noreferrer noopener">MathDataHub</a>
-                <a href="https://github.com/MathWebSearch/mws" target="_blank" rel="noreferrer noopener">MathWebSearch</a>
-              </div>
-            </div>
-          </div>
 
-          <div className="col s12 l6">
-
-            <div className="card">
+           <div className="card">
               <div className="card-content">
                 <span className="card-title">Other Associations</span>
                 <p>
@@ -134,30 +65,61 @@ export default class Home extends React.Component {
               </div>
             </div>
 
-
             <div className="card">
               <div className="card-content">
                 <span className="card-title">Contact</span>
-                You may best contact me by sending an email to <em>tom@tkw01536.de</em>.
-                I prefer plain text emails in english, but I also speak german.
-                <br />
-                My public PGP key has the fingerprint <em>CF21 1096 D46E 2650 6A1D 6F7B DABB 5DB5 A09C 64BA</em> and should be accessible from all of the commonly used keyservers.
-                <br />
-                If you want me to keep your message confidential, please send me a PGP encrypted email and I will respond accordingly.
-              </div>
-              <div className="card-action">
-                <a href="./tom@tkw01536.de.pub">Download PGP Key</a>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-content">
-                <span className="card-title">Legal</span>
+                <p>
+                  You may best contact me by sending an email to <em>tom@tkw01536.de</em>.
+                  I prefer plain text emails in english, but I also speak german.
+                </p>
                 <ClientSideScript src='https://inform.everyone.wtf/legal.min.js' data-site-id='4694798b-f1f9-43fe-a26a-5188d6e241ec' />
               </div>
             </div>
           </div>
+
+          <div className="col s12 l6">
+
+            <div className="card">
+              <div className="card-content">
+                <span className="card-title">Research Summary</span>
+                  <p>
+                    Modern science relies on an ever-increasing amount of data, a fact that is broadly acknowledged by politics, funding agencies and universities alike.
+                    Scientists author, curate, and eventually use or search through varying amounts of data and datasets, which frequently revolve around objects.
+                    Some examples of such objects include artifacts of cultural heritage or mathematical structures.
+                    <hr />
+                    It is well established that datasets should be Findable, Accessible, Interoperable and Reuseable - <em>FAIR</em> for short - in order to become useable in a broader and possibly public context.
+                    Typical research questions involve more than a single dataset, instead relying on <em>a large group of datasets that effectively act as a single (possibly federated) dataset</em>.
+                    Such <em>Research Data Commons</em> accelerate science and prevent duplicate datasets.
+                    <hr />
+                    I asked the following research questions:
+                    <ol>
+                      <li>
+                          How can researchers be enabled to answer their questions using a Research Data Commons?
+                      </li>
+                      <li>
+                          What types of software are required for a Research Data Commons? In particular, is it necessary to provide an explicit infrastructure?
+                      </li>
+                      <li>
+                          Which costs and benefits are associated with designing, developing, deploying and using a Research Data Commons?
+                      </li>
+                    </ol>
+                    <hr />
+                    To answer these questions, I explored the design space of Research Data Commons software by designing three systems - MathDataHub, the WissKI Distillery and the WissKI Data Viewer - which demonstrate that there is a disconnect between the costs and benefits of Research Data Commons.
+                    Costs need to be payed upfront at the beginning of a research project, whereas the benefits only come into effect much later.
+                    The latter are also conditional: They only arise if other researchers invest likewise.
+                    To minimize its effect, and enable the formation of Research Data Commons, it becomes necessary to decrease costs or to increase benefits.
+                  </p>
+                </div>
+
+              <div className="card-action">
+                <a href="https://github.com/MathHubInfo/mhd" target="_blank" rel="noreferrer noopener">MathDataHub</a>
+                <a href="https://github.com/fau-cdi/wisski-distillery" target="_blank" rel="noreferrer noopener">WissKI Distillery</a>
+                <a href="https://github.com/fau-cdi/hangover" target="_blank" rel="noreferrer noopener">WissKI Data Viewer</a>
+              </div>
+            </div>
+          </div>
         </div>
+        
     </>
   }
 }
