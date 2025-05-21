5f8b32d84fa21bbf41d8b74e70af45fd0bf54bff
Tom Wiesing <tkw01536@gmail.com>
2025-05-21 20:33:52 +0200

    migrate to vite

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'material-icons/iconfont/material-icons.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import './globals.css';
